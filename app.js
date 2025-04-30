b8ef5504505886f43422a9dc3842f621132228ee
Fahad Siyal <fahadsiyal748@gmail.com>
2025-04-30 18:27:53 +0500

    Update app.js

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ connectDB()
 app.use(express.static('public'));
 app.use(express.json())
 app.use(cors({
-  origin: 'https://e-comm-5xv9.vercel.app/', // allow Vite frontend
-  // origin: 'http://localhost:5173', // allow Vite frontend
-  credentials: true               // allow cookies / auth headers
+  // origin: 'https://e-comm-5xv9.vercel.app/', // allow Vite frontend
+  // // origin: 'http://localhost:5173', // allow Vite frontend
+  // credentials: true               // allow cookies / auth headers
 }));
 dotenv.config()
 
