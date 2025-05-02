30e07ac3086a5f285f52c5ffbc4f449ddeff3057
AarijFarhan <aarijhere10@gmail.com>
2025-05-02 07:26:45 -0700

    /

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ connectDB()
 app.use(express.static('public'));
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173', // allow Vite frontend
+  // origin: 'http://localhost:5173', // allow Vite frontend
+  origin: 'https://ecommfrontend-eight.vercel.app/', 
   credentials: true               // allow cookies / auth headers
 }));
 dotenv.config()
